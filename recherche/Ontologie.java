a54b754a7c6d934d67eddd8721645699b7e3bd7a
Bruno THIAOLAYEL <bruno.thiaolayel@gmail.com>
2014-12-17 21:47:49 +0100

    OWLAPI -> Jena, meilleure doc, à tester avec la recherche.

--- recherche/Ontologie.java
+++ recherche/Ontologie.java
@@ -1,192 +1,106 @@
 package projet.recherche;
 
-import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLAnnotation;
-import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLAnnotationProperty;
-import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLDataPropertyAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLEntity;
-import org.semanticweb.owlapi.model.OWLIndividual;
-import org.semanticweb.owlapi.model.OWLLiteral;
-import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom;
-import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.util.OWLOntologyWalker;
-import org.semanticweb.owlapi.util.OWLOntologyWalkerVisitor;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 
 public class Ontologie {
+	private OntModel model;
+	private String ontologyFile = "/Users/bruno/Dropbox/ISPED/inf203/atc.owl";
 	
-	//private String fileOntology = "/Users/bruno/Dropbox/ISPED/inf203/ISPED_RI/data/ontology/Thesaurus.owl";
-	//private String fileOntology = "/Users/bruno/Dropbox/ISPED/ime302_projet/final/animanga.owl";
-	private String fileOntology = "/Users/bruno/Downloads/aero.owl"; 
-	// aero utilise des ontologies sur le web dont : iao.owl
-	// Lors de l'exécution il se peut qu'une exception 'socket error' intervienne <-- embettant
-
-	private OWLOntology ontology;
-	private OWLOntologyManager manager;
-	private String defaultURI;
-	
-	public Ontologie (){		
-		//System.setProperty("entityExpansionLimit","100000000");
-		try {	
-			IRI iri;
-			long startTime, endTime, duration;
-			defaultURI = "ontology does not have an IRI";
-
-			startTime = System.nanoTime();
-			
-			manager = OWLManager.createOWLOntologyManager();
-			iri = IRI.create(new File(fileOntology));
-			ontology = manager.loadOntologyFromOntologyDocument(iri);
-			
-			endTime = System.nanoTime();
-			duration = (endTime - startTime) / 1000000; 
-
-			// TODO
-			// Problème : getOntologyIRI / getDefaultDocumentIRI
-			// peuvent ne pas donner le bon URI ...
-			// Certaines ontologies donnent http://purl.obolibrary.org/obo/
-			// d'autres http://purl.obolibrary.org/obo/aero.owl
-			// Les séparateurs peuvent être différents aussi par ontologies : / ou #
-			
-			//IRI uri = ontology.getOntologyID().getOntologyIRI();
-			IRI uri = ontology.getOntologyID().getDefaultDocumentIRI();
-
-			//if (uri != null)
-			//	defaultURI = uri.toString();
-			
-			defaultURI = "http://purl.obolibrary.org/obo/";
-			
-			System.out.println("Loaded ontology: " + ontology);
-			System.out.println("Temps de chargement  : "+ duration + " ms");
-			System.out.println(defaultURI);
-
-		} catch (OWLOntologyCreationException e) {
-			e.printStackTrace();
-		}
+	public Ontologie() {
+		OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
+		InputStream in = FileManager.get().open(ontologyFile);
+		
+		if (in == null)
+			throw new IllegalArgumentException("Fichier non existant");
+		this.model = (OntModel) model.read(in, null, "TTL");
 	}
-
+	
 	public List<String> expansionRequete(List<String> motsEntres) {
+		// Return a list of new keywords associated by entry keywords from ontology
+		
+		List<String> motsEntresEtendus = new ArrayList<String>();
+		ExtendedIterator<OntClass> topLevels = model.listClasses();
+		OntClass cur;
+		while(topLevels.hasNext()) {
+			cur = topLevels.next(); 
+			String libelle = cur.getLabel(null);
+			if (isToKeepAsMatch(libelle, motsEntresEtendus))
+				motsEntresEtendus.addAll(getBranchAnnotated(cur));
+			motsEntresEtendus.addAll(recTree(cur, motsEntres));
+		}
+		return new ArrayList<String>(new HashSet<String>(motsEntresEtendus)); // Ugly way to clear all occurences
+	}
+	
+	private List<String> getBranchAnnotated(OntClass cur) {
+		// Get all the branches labels into a string list
 		List<String> res = new ArrayList<String>();
 		
-		OWLDataFactory factory = this.manager.getOWLDataFactory();
-		OWLClass curClass;
-		IRI tmp;
-		
-		// On récupère directement les annotations si un concept est évident
-		// évident : si un mot entré correspond exactement à un concept de l'ontologie
-		
-		// TODO
-		// Problème : censé fonctionner avec AERO_0000358 qui est un concept d'aero.owl mais non.
-		
-		String sep = "/";
-//		String sep = "#";
-		for (String cur: motsEntres) {
-			tmp = IRI.create(this.defaultURI+sep+cur);
-			if (this.ontology.containsClassInSignature(tmp)) { // test si le concept existe
-				curClass = factory.getOWLClass(tmp);
-				for (OWLAnnotation annotation : curClass.getAnnotations(this.ontology, factory.getRDFSLabel())) {
-					// Parcours les annotations
-					if (annotation.getValue() instanceof OWLLiteral) {
-						OWLLiteral val = (OWLLiteral) annotation.getValue();
-						res.add(val.getLiteral());
-					}
-				}
-			}
+		ExtendedIterator<OntClass> parents = cur.listSuperClasses();
+		ExtendedIterator<OntClass> childs = cur.listSubClasses(); 
+
+		OntClass tmp;
+		String libelle;
+		while(parents.hasNext()) {
+			tmp = parents.next();
+			libelle = tmp.getLabel(null);
+			res.add(libelle);
 		}
-		
-		// Visiteur qui marche sur un chemin sur l'ontologie
-		// L'idée : s'arrêter sur les noeuds qui ont des annotations, et voir si ils contiennent 
-		// au moins un des mots de la liste motsEntres
-		// si oui : on rajoute le concept associé à la liste 'res' étendue
-		
-		// TODO
-		// Problèmes : si il y a plusieurs annotations, il s'arrête qu'à la première.
-		// je ne sais toujours pas comment récupérer le concept (OWLClass et son nom)
-		// qui est associé à l'annotation dont on se situe lors du parcours du visiteur
-		
-		/*
-		final OWLAnnotationProperty comment = factory.getRDFSComment();
-		OWLOntologyWalker walker = new OWLOntologyWalker(Collections.singleton(ontology));
-
-		OWLOntologyWalkerVisitor visitor = new OWLOntologyWalkerVisitor(walker) {
-			@Override
-			public Object visit(OWLObjectPropertyAssertionAxiom axiom) {
-				System.out.println(axiom);
-				return null;
-			}
-			@Override
-			public Object visit(OWLDataPropertyAssertionAxiom axiom) {
-				System.out.println(axiom);
-				return null;
-			}
-			@Override
-			public Object visit(OWLAnnotationAssertionAxiom axiom) {
-				System.out.println("TOTO");
-				
-				for (OWLEntity toto: axiom.getSignature()) {
-					System.out.println(toto + "TITIITTIITITITITITITITI");
-				}
-				System.out.println(axiom.getValue());
-
-				String ref = axiom.getValue().toString();
-				boolean flag = false;
-				for (String ew: motsEntres) {
-					if (ref.contains(ew)) {
-						flag = true;
-						break;
-					}
-				}
-
-				if (flag) {
-					for (OWLEntity cur: axiom.getSignature())
-						if (cur.isOWLClass()) {
-							OWLClass tmp = cur.asOWLClass();
-							for(OWLAnnotationAssertionAxiom cur2: tmp.getAnnotationAssertionAxioms(ontology))
-								res.add(cur2.getValue().toString());
-							System.out.println(res);
-						}
-					res.add(ref);
-				}
-
-				System.out.println("TITI");
-				return null;
-			}
-			
-			@Override
-			public Object visit(OWLObjectSomeValuesFrom desc) {
-				//System.out.println(desc);
-				//System.out.println("         " + getCurrentAxiom());
-				//System.out.println("TOTOTOTOOTOT" + getCurrentAxiom());
-				return null;
-			}
-			
-		};
-		walker.walkStructure(visitor);      
-*/
+		while(childs.hasNext()) {
+			tmp = childs.next();
+			libelle = tmp.getLabel(null);
+			res.add(libelle);
+		}
 		return res;
 	}
 	
-	public static void main(String args[]) {
-		Ontologie ontology = new Ontologie();
-		List<String> motsEntres = new ArrayList<String>();
-		motsEntres.add("anaphylaxis");
-		motsEntres.add("cardiovascular");
-		motsEntres.add("AERO_0000358");
+	private boolean isToKeepAsMatch(String libelle, List<String> motsEntres) {
+		// Return if at least one word from motsEntres get a match from a libelle (per word)
+		// Suppose that libelle is not already splitted
+		// AND motsEntres is a list of unique words
+		List<String> libelleSplit = new ArrayList<String>(Arrays.asList(libelle.split(" ")));
+		
+		for (String keyWord: motsEntres)
+			for (String split: libelleSplit)
+				if (split.equals(keyWord)) // Match exactly a word from entry word to a labeled concept from ontology
+					return true;
+		return false;
+	}
+	
+	private List<String> recTree(OntClass root, List<String> motsEntresOrig) {
+		// Browse all subclasses 
+		List<String> res = new ArrayList<String>();
+		
+		ExtendedIterator<OntClass> subClasses = root.listSubClasses();
+		OntClass cur;
+		while(subClasses.hasNext()) {
+			cur = subClasses.next(); 
+			String libelle = cur.getLabel(null);
 
-		List<String> conceptsEtendus = ontology.expansionRequete(motsEntres);
-		System.out.println(conceptsEtendus);
+			if (isToKeepAsMatch(libelle, motsEntresOrig))
+				res.addAll(getBranchAnnotated(cur));
+			res.addAll(recTree(cur, motsEntresOrig));
+		}
+		return res;
+	}
+	
+	public static void main(String[] args){
+		Ontologie onto = new Ontologie();
+		List<String> motsEntres = new ArrayList<String>();
+		motsEntres.add("Benproperine");
+		motsEntres.add("spironolactone");
+
+		List<String> res = onto.expansionRequete(motsEntres);
+		System.out.println(res);
 	}
 }
